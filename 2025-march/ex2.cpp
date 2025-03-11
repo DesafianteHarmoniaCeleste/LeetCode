82752c25eca1904f5538e88f5c82361af14d1ef0
Pipoquinha <lemepedro2014@gmail.com>
2025-03-11 19:23:05 -0300

    minor changes

--- 2025-march/ex2.cpp
+++ 2025-march/ex2.cpp
@@ -27,7 +27,9 @@ struct ListNode{
         - the last number being a carry
     i Had some help from Chatgpt, where i was using the construction fot returned_list wrong
 
-    using Ifs inside the main while was a smart idea, instead of completing the rest of the list outside of it using 2 if's
+    using Ifs inside the main while was a smart idea, instead of completing the rest of the list outside of the main while using 2 if's
+    
+    From this exercise i learned a little more about Simple-linkedLists in c++ ( i was more familiar with it in c ), and the CORRECT use of pointers
 */
 class Solution{
     public:
@@ -38,7 +40,7 @@ class Solution{
             ListNode* current_node = returned_list;
 
             while(l1 || l2 || carry){
-                int sum = carry;
+                int sum= carry;
                 if(l1){
                     sum += l1->val;
                     l1 = l1->next;
@@ -48,9 +50,9 @@ class Solution{
                     l2 = l2->next;
                 }
     
-                carry = sum / 10;
-                current_node->next = new ListNode(sum % 10);
-                current_node = current_node->next;
+                carry= sum/10;
+                current_node->next = new ListNode(sum%10);
+                current_node= current_node->next;
             }
 
             return returned_list->next;
