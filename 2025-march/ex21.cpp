5835e0d0d144653dca6f60e5da833920dc39428d
Pipoquinha <lemepedro2014@gmail.com>
2025-03-16 16:14:46 -0300

    100 / 62 %

--- 2025-march/ex21.cpp
+++ 2025-march/ex21.cpp
@@ -24,13 +24,13 @@ struct ListNode {
     };
 
 /*
-
+    Merging 2 sorted lists
+    from this problem i learned a little more of pointers
 */
 
 
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
-        // variable declarations
-
+    // variable declarations
     ListNode* returned_list = new ListNode();
     ListNode* current_node = returned_list;
 
@@ -40,43 +40,39 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
             list1= list1->next;
         }
         else{
-            current_node->next = list2;
-            list2 = list2->next;
+            current_node->next= list2;
+            list2= list2->next;
         }
-        current_node = current_node->next;
+        current_node= current_node->next;
     }
 
-    if(list1){
-        current_node->next = list1;
-    }
-    if(list2){
-        current_node->next = list2;
-    }
-            
+    if(list1){ current_node->next = list1; }
+    if(list2){ current_node->next = list2; }
+
     return returned_list->next;
 }
 
-// Função auxiliar para imprimir a lista
+// debbug function
 void printList(ListNode* head){
-    while (head) {
-        std::cout << head->val << " ";
-        head = head->next;
+    while(head){
+        std::cout<< head->val << " ";
+        head= head->next;
     }
     std::cout << std::endl;
 }
 
-// Teste
+// debbug main to test
 int main(){
     ListNode* l1 = new ListNode(2);
-    l1->next = new ListNode(4);
-    l1->next->next = new ListNode(3);
+    l1->next = new ListNode(3);
+    l1->next->next = new ListNode(4);
 
     ListNode* l2 = new ListNode(5);
     l2->next = new ListNode(6);
-    l2->next->next = new ListNode(4);
+    l2->next->next = new ListNode(7);
 
     ListNode* result = mergeTwoLists(l1, l2);
-    printList(result); // Output: 7 0 8
+    printList(result);
 
     return 0;
 }
